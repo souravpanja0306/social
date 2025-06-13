93418ecd3754a41a655479a9aa7e6f6d7cc0a8b3
souravpanja <panjasourav03@gmail.com>
2025-06-14 01:26:24 +0530

    New Bug Fixed

--- src/Pages/Post/CreatePost.jsx
+++ src/Pages/Post/CreatePost.jsx
@@ -25,28 +25,32 @@ const CreatePost = () => {
       <PageHeader title={`Create Post`} />
 
       <div className=''>
-        <textarea className='border border-green-500 rounded-lg w-full p-2 active' placeholder="What's on your mind?" />
+        <textarea className='capitalize border border-green-500 rounded-lg w-full p-2 active' placeholder="What's on your mind?" />
       </div>
-      <div className='min-h-48 border border-dashed border-green-500 rounded-lg flex justify-center items-center' onClick={handleClick}>
-        {image ? (
-          <img src={image} alt="Preview" className="object-cover w-full h-full" />
-        ) : (
-          <div className='flex flex-col justify-center items-center'>
-            <BsPlusLg className='text-green-500 text-xl font-semibold' />
-            <span className='text-green-500'>Click here to upload...</span>
-          </div>
-        )
+      <div className='p-1 min-h-48 border border-dashed border-green-500 rounded-lg flex justify-center items-center' onClick={handleClick}>
+        {
+          image
+            ? (
+              <img src={image} alt="Preview" className="object-cover w-full h-full rounded-lg" />
+            )
+            : (
+              <div className='flex flex-col justify-center items-center'>
+                <BsPlusLg className='text-green-500 text-xl font-semibold' />
+                <span className='text-green-500'>Click here to upload...</span>
+              </div>
+            )
         }
 
         <input
           type="file"
           ref={fileInputRef}
           className='hidden'
-          // onChange={(e) => {
-          //   console.log("Selected file:", e.target.files[0]);
-          // }}
           onChange={handleFileChange}
         />
+      </div>
+      <div className='py-2 flex justify-center items-center gap-2'>
+        <button className='p-2 bg-green-500 rounded-lg'>Submit</button>
+        <button className='p-2 bg-red-500 rounded-lg'>Discard</button>
       </div>
     </div>
   )
